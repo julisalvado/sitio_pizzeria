9ef136f89ea6ae10c56f8ea32790f191eb3bd5fc
Julieta Salvado <julietasalvad@gmail.com>
2025-03-24 22:39:23 -0300

    tarea m4u1

---
+++ pizzeria/frontend/my-app/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer>
+            <p>Derechos reservados. La Ramona 2025.</p>
+        </footer>
+    );
+}
+
+export default Footer;

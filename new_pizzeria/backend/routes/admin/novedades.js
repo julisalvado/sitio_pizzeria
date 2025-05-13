601038d6bcb0c3d2c8fc47013b5a036b27ac3a0f
Julieta Salvado <julietasalvad@gmail.com>
2025-05-13 09:25:49 -0300

    m6u1

--- new_pizzeria/backend/routes/admin/novedades.js
+++ new_pizzeria/backend/routes/admin/novedades.js
@@ -1,12 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin/novedades', {    //login.hbs
-    layout:'admin/layout',      //layout.hbs
-    persona: req.session.nombre
-  });
+router.get('/', async function(req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+        
+        res.render('admin/novedades', {    //login.hbs
+            layout:'admin/layout',      //layout.hbs
+            persona: req.session.nombre,
+            novedades
+        });
 });
+
+/*para mostrarme el formulario o diseño de agregar*/
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        console.log(req.body);
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
 
 module.exports = router;
